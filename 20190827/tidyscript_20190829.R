0b58d295b4a8eca9e446d92a9d55d12975b8c14f
Danielle Becker <danielle.becker.927@my.csun.edu>
2019-08-27 18:32:38 -0700

    commit changes

--- 20190827/tidyscript_20190829.R
+++ 20190827/tidyscript_20190829.R
@@ -1,2 +1,14 @@
 #First day of tidy tuesday
-a <- rnorm(n = 100, mean = 10, sd = 2)
+#Danielle Becker
+#20190827
+
+
+#load libraries needed
+library(tidyverse)
+
+#download simpsons data to work with for tidy tuesday
+simpsons <- readr::read_delim("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-08-27/simpsons-guests.csv", delim = "|", quote = "")
+view(simpsons)
+
+#how often a guest star appears in episodes listed
+
